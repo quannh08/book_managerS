791fd90d67db28840387031b7d1f81fb48dc7fc4
QuanNM1903 <126430742+QuanNM1903@users.noreply.github.com>
2024-10-27 00:15:57 +0700

    Update models.py

--- authors/models.py
+++ authors/models.py
@@ -5,7 +5,7 @@ class Author(models.Model):
     biography = models.TextField(blank=True, null=True)  
     birth_date = models.DateField(null=True, blank=True)  
     nationality = models.CharField(max_length=100, null=True, blank=True)  
-    profile_picture = models.ImageField(upload_to='authors/', null=True, blank=True)  # Ảnh đại diện
+    profile_picture = models.ImageField(upload_to='authors/', null=True, blank=True) 
     awards = models.TextField(blank=True, null=True)  
     books = models.JSONField(blank=True, null=True)  
 
