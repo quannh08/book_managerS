475a673396829424eb82dc427a746b0b8473a4b7
Hoàng Lê Đức <DucHL.B22CN224@stu.ptit.edu.vn>
2024-10-26 07:35:18 +0700

    models.py

--- books/models.py
+++ books/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    ID_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField()
+    content = models.TextField()
+    image = models.ImageField(upload_to='book_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
