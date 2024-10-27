d2c22a059293da13bf5ccbfd00ebfbfd47bba47c
QuanNM1903 <126430742+QuanNM1903@users.noreply.github.com>
2024-10-27 18:58:18 +0700

    Update serializers.py

--- authors/serializers.py
+++ authors/serializers.py
@@ -5,4 +5,4 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['id', 'name', 'biography', 'birth_date', 'nationality', 
-                  'profile_picture', 'awards', 'books'] 
+                  'profile_picture', 'awards'] 
