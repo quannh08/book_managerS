3a63b8b9b7203564a89e2800621644f6f03a2e8d
Hoàng Lê Đức <DucHL.B22CN224@stu.ptit.edu.vn>
2024-10-31 21:26:32 +0700

    views.py

--- books/views.py
+++ books/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, Category, Author
+from .serializers import BookSerializer, CategorySerializer, AuthorSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
