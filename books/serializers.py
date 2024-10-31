c6f693c266bba8b764de9e1793cda3627ee14d16
Hoàng Lê Đức <DucHL.B22CN224@stu.ptit.edu.vn>
2024-10-31 21:25:54 +0700

    serializers.py

--- books/serializers.py
+++ books/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, Category, Author
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = [ 'name','id' ]
+        
 class BookSerializer(serializers.ModelSerializer):
+    ID_category = CategorySerializer()
+    id_author = AuthorSerializer()
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ['id', 'ID_category', 'id_author', 'title', 'description', 'content', 'image']
