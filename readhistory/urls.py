ed343f440c53689a4001a0d3b6ac3551b79f4d21
NgP <phunthytl@gmail.com>
2024-11-04 21:14:42 +0700

    Update urls.py

--- readhistory/urls.py
+++ readhistory/urls.py
@@ -2,5 +2,5 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('api/read-history/', ReadHistoryAPIView.as_view(), name='api-read-history'),
+    path('read-history/', ReadHistoryAPIView.as_view(), name='api-read-history'),
 ]
