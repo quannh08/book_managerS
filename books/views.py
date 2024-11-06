177fc3e07f6d244efa00c925b5fbca342d1baee4
buuinam <buivannam13032004@gmail.com>
2024-11-06 14:52:18 +0700

    views.py

--- books/views.py
+++ books/views.py
@@ -7,20 +7,19 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    def partial_update(self, request, *args, **kwargs):
-        # Lấy đối tượng book theo primary key (pk)
+    # Phương thức để tăng số lần đọc khi nhận request PATCH
+    @action(detail=True, methods=['patch'], url_path='increase-read-count')
+    def increase_read_count(self, request, pk=None):
+        # Lấy đối tượng sách theo primary key (pk)
         instance = self.get_object()
-        # Nếu có tham số 'read_count' trong request, cập nhật số lần đọc
-        if 'read_count' in request.data:
-            try:
-                # Tăng số lần đọc bằng giá trị read_count gửi lên
-                instance.read_count += int(request.data['read_count'])
-                instance.save()  # Lưu thay đổi vào cơ sở dữ liệu
-                return Response({"status": "read_count updated", "read_count": instance.read_count}, status=status.HTTP_200_OK)
-            except ValueError:
-                return Response({"error": "Invalid read_count value"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        return super().partial_update(request, *args, **kwargs)
+        # Tăng số lần đọc thêm 1
+        instance.read_count += 1
+        instance.save()  # Lưu thay đổi vào cơ sở dữ liệu
+        # Trả về kết quả sau khi cập nhật
+        return Response(
+            {"status": "Read count increased", "read_count": instance.read_count},
+            status=status.HTTP_200_OK
+        )
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
